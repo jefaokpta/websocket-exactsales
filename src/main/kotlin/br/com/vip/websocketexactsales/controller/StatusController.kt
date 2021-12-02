5f56424724c35f90aa3ac9a7abf8a8db8fde80fd
jefaokpta <jefaokpta@hotmail.com>
2021-12-02 11:43:49 -0300

    td funcionando ok

--- src/main/kotlin/br/com/vip/websocketexactsales/controller/StatusController.kt
+++ src/main/kotlin/br/com/vip/websocketexactsales/controller/StatusController.kt
@@ -1,11 +1,13 @@
 package br.com.vip.websocketexactsales.controller
 
 import br.com.vip.websocketexactsales.model.Status
+import br.com.vip.websocketexactsales.model.WsSessionCentral
 import br.com.vip.websocketexactsales.websocket.handler.WsSoftphoneHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 /**
  * @author Jefferson Alves Reis (jefaokpta) < jefaokpta@hotmail.com >
@@ -17,4 +19,7 @@ class StatusController(private val wsSoftphoneHandler: WsSoftphoneHandler) {
 
     @GetMapping("/{session}")
     fun consulta(@PathVariable session: String) = wsSoftphoneHandler.getStatus(Status(session = session))
+
+    @GetMapping
+    fun listaSessoes() = Mono.justOrEmpty(WsSessionCentral.sessions.keys.toList())
 }
